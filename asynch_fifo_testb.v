1eedf33b2ff7a6ee3450ec71908a5d45d9fbfd24
yagyash07 <sharmayagya088l@gmail.com>
2025-07-04 22:59:45 +0530

    Update asynch_fifo_testb.v

--- asynch_fifo_testb.v
+++ asynch_fifo_testb.v
@@ -4,53 +4,52 @@ module async_fifo_tb();
     parameter depth = 8;
     parameter d_width = 8;
 
-    reg wr_clk;
-    reg rd_clk;
+    reg wclk;
+    reg rclk;
     reg reset;
-    reg wr_en;
-    reg rd_en;
-    wire full_o;
-    wire empty_o;
+    reg w_en;
+    reg r_en;
+    wire full;
+    wire empty;
     reg [d_width-1:0] wr_data;
     wire [d_width-1:0] rd_data;
 
     // DUT instantiation
     async_fifo #(depth, d_width) dut (
-        .wr_clk(wr_clk),
-        .rd_clk(rd_clk),
+        .wclk(wclk),
+        .rclk(rclk),
         .reset(reset),
-        .wr_en(wr_en),
-        .rd_en(rd_en),
+        .w_en(w_en),
+        .r_en(r_en),
         .wr_data(wr_data),
         .rd_data(rd_data),
-        .full_o(full_o),
-        .empty_o(empty_o)
+        .full(full),
+        .empty(empty)
         
     );
         
     // Clocks
-    always #5 wr_clk = ~wr_clk;
-    always #10 rd_clk = ~rd_clk;
+    always #5 wclk = ~wclk;
+    always #10 rclk = ~rclk;
     initial begin
-    wr_clk = 0;
-    rd_clk = 0;
+    wclk = 0;
+    rclk = 0;
     reset = 1;
-    wr_en = 0;
-    rd_en = 0;
+    w_en = 0;
+    r_en = 0;
     wr_data = 0;
     #25;
 
- reset=1'b0; wr_en=1;rd_en=0;wr_data=8'd45;#25
- reset=1'b0;  wr_en=1;rd_en=0;wr_data=8'd23;#25
- reset=1'b0;  wr_en=1;rd_en=0;wr_data=8'd27;#25
- reset=1'b0;  wr_en=1;rd_en=0;wr_data=8'd22;#25
- reset=1'b0;  wr_en=1;rd_en=0;wr_data=8'd12;#25   
- reset=1'b0;  wr_en=0;rd_en=1;wr_data=8'd27;#25   
- reset=1'b0;  wr_en=0;rd_en=1;wr_data=8'd22;#25 
- reset=1'b0;  wr_en=0;rd_en=1;wr_data=8'd14;#25 
+ reset=1'b0; w_en=1;r_en=0;wr_data=8'd45;#25
+ reset=1'b0;  w_en=1;r_en=0;wr_data=8'd23;#25
+ reset=1'b0;  w_en=1;r_en=0;wr_data=8'd27;#25
+ reset=1'b0;  w_en=1;r_en=0;wr_data=8'd22;#25
+ reset=1'b0;  w_en=1;r_en=0;wr_data=8'd12;#25   
+ reset=1'b0;  w_en=0;r_en=1;wr_data=8'd27;#25   
+ reset=1'b0;  w_en=0;r_en=1;wr_data=8'd22;#25 
+ reset=1'b0;  w_en=0;r_en=1;wr_data=8'd14;#25 
  reset=1'b1;#25
  $finish;
  
  end
  endmodule  
-
