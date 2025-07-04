1ba59aec3816156208d300d7717cd0011ea372be
yagyash07 <sharmayagya088l@gmail.com>
2025-07-04 15:50:21 +0530

    Update dual_mm.v

--- dual_mm.v
+++ dual_mm.v
@@ -4,9 +4,10 @@ module dual_mm_port #(parameter DEPTH=8)(
     input wr_en_i,
     input rd_en_i,
     input [7:0] data_i,
-    input[2:0] wr_addr;
-    input [2:0] rd_addr;
-    output reg [7:0] data_o
+    input [2:0] wr_addr,
+    input [2:0] rd_addr,
+    output reg [7:0] data_o,
+    input full,empty
 
     
 );
@@ -14,13 +15,13 @@ reg [7:0] mem[0:DEPTH-1];
 
 
 always @(posedge wr_clk) begin
-    if (wr_en_i) begin
+    if (wr_en_i && ~full) begin
         mem [wr_addr] <=data_i;
     end
 end
 
 always @(posedge rd_clk) begin
-    if (rd_en_i) begin
+    if (rd_en_i && ~empty) begin
         data_o<=mem[rd_addr];
     end
 end
